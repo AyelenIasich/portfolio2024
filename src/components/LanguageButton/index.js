34a2542921becadb8b67734bf644231a69143f1d
AyelenIasich <ayeleniasich@gmail.com>
2024-10-09 23:16:54 -0300

    Style: change language button styles

--- src/components/LanguageButton/index.js
+++ src/components/LanguageButton/index.js
@@ -10,9 +10,9 @@ function LanguageButton() {
     const newLanguage = i18n.language === "en" ? "es" : "en";
     i18n.changeLanguage(newLanguage);
   };
-  
+
   return (
-    <div  onClick={toggleLanguage}  className="language-button">
+    <div  onClick={toggleLanguage}  className="language-btn">
       <button type="button" className="btn-lang">
         <FaGlobeAmericas className="icon-lang"/>
       </button>
