e3e1a8cfe7cdbff795191a2a73812148328811b4
AyelenIasich <ayeleniasich@gmail.com>
2024-10-11 23:40:56 -0300

    Chore: Adjustments in index.html to improve SEO

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -24,8 +24,8 @@ function Home() {
 
   return (
     <section className="home-section">
-      <BgHomeSVG className="background-svg d-none d-md-block" />
-      <BgHomeMobile className="background-svg  d-md-none" />
+      <BgHomeSVG className="background-svg d-none d-lg-block" />
+      <BgHomeMobile className="background-svg  d-lg-none" />
       <div className="container d-flex  align-items-center justify-content-center ">
         <div className="row content-row ">
           <div className="col-12 col-lg-6 align-self-center justify-content-center order-2 text-content col-home ps-4">
