23a92a24b3ba35fc3bbf05850008aee56a9f1612
AyelenIasich <ayeleniasich@gmail.com>
2024-12-01 22:17:50 -0300

    Hotfix: translation of education cards

--- src/sections/Education/Education.js
+++ src/sections/Education/Education.js
@@ -11,14 +11,7 @@ import EmptyEducationCard from "../../components/EmptyEducationCard/EmptyEducati
 function Education() {
   const { t } = useTranslation();
   const [visibleItems, setiVisibleItems] = useState(4);
-  const [isLoading, setIsLoading] = useState(true);
-  const [education, setEducation] = useState([]);
-
-  useEffect(() => {
-    const educationInfo = educationData(t);
-    setEducation(educationInfo);
-    setIsLoading(false);
-  }, []);
+  const educationInfo = educationData(t);
 
   const handleShowMore = () => {
     setiVisibleItems((prevVisibleItems) => prevVisibleItems + 4);
@@ -30,35 +23,35 @@ function Education() {
         <div className="col-12 text-center pt-5 pt-xxl-5 mt-xxl-3">
           <TitleSection t={t} title={t("EducationTitle")} />
         </div>
-          <div className="col-12 col-md-10 col-lg-9 col-xxl-8  mx-auto">
-            <p className="results-counter">
-              {t("ShowingResults", {
-                current: Math.min(visibleItems, education.length),
-                total: education.length,
-              })}
-            </p>
-          </div>
-          <div className="col-12 col-md-10 col-lg-9 col-xxl-8  mx-auto">
-            {isLoading
-              ? Array.from({ length: 4 }).map((_, index) => (
-                  <EmptyEducationCard key={index} />
-                ))
-              : education
-                  .slice(0, visibleItems)
-                  .map((ed, index) => <EducationCard key={index} {...ed} />)}
-
-            {visibleItems < education.length && (
-              <div className="col-12 text-center">
-                <SecondaryBtn
-                  label={t("BtnShowMore")}
-                  onClick={handleShowMore}
-                  isContactBtn={false}
-                  extraStyle="btn-show-more"
-                />
-              </div>
-            )}
-          </div>
+        <div className="col-12 col-md-10 col-lg-9 col-xxl-8  mx-auto">
+          <p className="results-counter">
+            {t("ShowingResults", {
+              current: Math.min(visibleItems, educationInfo.length),
+              total: educationInfo.length,
+            })}
+          </p>
         </div>
+        <div className="col-12 col-md-10 col-lg-9 col-xxl-8  mx-auto">
+          {educationInfo
+            ? educationInfo
+                .slice(0, visibleItems)
+                .map((ed, index) => <EducationCard key={index} {...ed} />)
+            : Array.from({ length: 4 }).map((_, index) => (
+                <EmptyEducationCard key={index} />
+              ))}
+
+          {visibleItems < educationInfo.length && (
+            <div className="col-12 text-center">
+              <SecondaryBtn
+                label={t("BtnShowMore")}
+                onClick={handleShowMore}
+                isContactBtn={false}
+                extraStyle="btn-show-more"
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </section>
   );
 }
