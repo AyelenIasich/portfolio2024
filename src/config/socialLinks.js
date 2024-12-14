fc40b94daf3cf91e8ca7c96c83fa8204a66d519d
AyelenIasich <ayeleniasich@gmail.com>
2024-12-14 17:47:26 -0300

    Fix: Adjustments in cv data 2

--- src/config/socialLinks.js
+++ src/config/socialLinks.js
@@ -1,6 +1,6 @@
 export const socialLinks = {
     github: "https://github.com/AyelenIasich",
     linkedin: "https://ar.linkedin.com/in/ayeiasich",
-    gmail:"ayeleniasich92@gmail.com"
+    gmail:"iasichayelen@gmail.com"
   };
   
--- src/utils/cvUtils.js
+++ src/utils/cvUtils.js
@@ -1,8 +1,8 @@
 export const handleDownloadCV = (language) => {
   const cvUrl =
     language === "es"
-      ? "https://drive.google.com/file/d/1n9DFid7lGlGzn9uMY7EpkRpz4GQsvlUl/view?usp=sharing" // cv spanish
-      : "https://drive.google.com/file/d/1Pn8OqAzcxS7w0ie5pHke17TndPbgIOIQ/view?usp=sharing"; // cv english
+      ? "https://drive.google.com/file/d/1MRo-jU20nSDkXg1Jjb_sTGIqerBd7I02/view?usp=sharing" // cv spanish
+      : "https://drive.google.com/file/d/1S14nYFpnpenh-hj0xI6Y_rZARgRl5sOu/view?usp=sharing"; // cv english
 
   window.open(cvUrl, "_blank");
 };
