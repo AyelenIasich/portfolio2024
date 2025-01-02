525d157f50240be2c1d2768881fcb56db5d568bf
AyelenIasich <ayeleniasich@gmail.com>
2025-01-02 19:56:11 -0300

    Fix: new cv link for cv download

--- src/utils/cvUtils.js
+++ src/utils/cvUtils.js
@@ -1,8 +1,8 @@
 export const handleDownloadCV = (language) => {
   const cvUrl =
     language === "es"
-      ? "https://drive.google.com/file/d/11yJYIjsGPs3WXwfeJ452mkH-OamJU8lz/view?usp=sharing" // cv spanish
-      : "https://drive.google.com/file/d/13dDcDDjjgPx72LR8WqkQqhBlYtXoTz-V/view?usp=sharing"; // cv english
+      ? "https://drive.google.com/file/d/1olCw6sNu3xJ8FFCZVlGrl2kbMB3L2KfJ/view?usp=sharing" // cv spanish
+      : "https://drive.google.com/file/d/1HGBEVFISTMBT-5zCED5bQ3g0bQ_5MynP/view?usp=sharing"; // cv english
 
   window.open(cvUrl, "_blank");
 };
