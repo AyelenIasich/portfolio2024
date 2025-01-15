0303da2962964e588abe6bf8fe6340711f42cca3
AyelenIasich <ayeleniasich@gmail.com>
2025-01-15 14:47:50 -0300

    Feat: add new recomendation

--- src/Data/testimonialData.js
+++ src/Data/testimonialData.js
@@ -1,25 +1,31 @@
-import IvanBevaqua from "../assets/png/testimonial/Ivan.jpg";
-import LucasRivero from "../assets/png/testimonial/lucas.jpg";
-
+import IvanBevaqua from '../assets/png/testimonial/Ivan.jpg'
+import LucasRivero from '../assets/png/testimonial/lucas.jpg'
+import GioRangel from '../assets/png/testimonial/gio.jpg'
 
 const testimonialData = (t) => [
   {
-    name: "Lucas Riveros",
-    title: "Full Stack Developer || Odoo Developer",
-    feedback1:t("Testimonial-feedback1-1"),
-    feedback2: t("Testimonial-feedback1-2"),
-    image: LucasRivero,
-    linkelind:"https://www.linkedin.com/in/lucas-riveros-861036180/"
-
+    name: 'Giovana Rangel',
+    title: 'Sr Software Engineer NodeJs - ReactJS',
+    feedback1: t('Testimonial-feedback3-1'),
+    image: GioRangel,
+    linkelind: 'https://ar.linkedin.com/in/giovana-rangel-4974a51a9',
   },
   {
-    name: "Iván Bevaqua",
-    title: t("Testimonial-role2"),
-    feedback1: t("Testimonial-feedback2-1"),
-    feedback2:  t("Testimonial-feedback2-2"),
-    image: IvanBevaqua,
-    linkelind:"https://www.linkedin.com/in/bevaquaivan/"
+    name: 'Lucas Riveros',
+    title: 'Full Stack Developer || Odoo Developer',
+    feedback1: t('Testimonial-feedback1-1'),
+    feedback2: t('Testimonial-feedback1-2'),
+    image: LucasRivero,
+    linkelind: 'https://www.linkedin.com/in/lucas-riveros-861036180/',
   },
-];
+  {
+    name: 'Iván Bevaqua',
+    title: t('Testimonial-role2'),
+    feedback1: t('Testimonial-feedback2-1'),
+    feedback2: t('Testimonial-feedback2-2'),
+    image: IvanBevaqua,
+    linkelind: 'https://www.linkedin.com/in/bevaquaivan/',
+  },
+]
 
-export default testimonialData;
+export default testimonialData
