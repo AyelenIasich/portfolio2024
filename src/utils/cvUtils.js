966740e78ceb8ea640ad0f9b37009b2bcba0f3ce
AyelenIasich <ayeleniasich@gmail.com>
2026-03-11 18:57:24 -0300

    new cvs

--- src/utils/cvUtils.js
+++ src/utils/cvUtils.js
@@ -1,8 +1,8 @@
 export const handleDownloadCV = (language) => {
   const cvUrl =
     language === "es"
-      ? "https://drive.google.com/file/d/1ggtnaNIgspyEn4_NOoMVZjCXcUQXjRi1/view?usp=drive_link" // cv spanish
-      : "https://drive.google.com/file/d/1jlta-3PPlMCNfmC3RfNV28maChdoZxvC/view?usp=drive_link"; // cv english
+      ? "https://drive.google.com/file/d/1nysXsQhGkEfZb_2cZwBHGjb6RL-Ph4Un/view?usp=drive_link" // cv spanish
+      : "https://drive.google.com/file/d/1C5lwJNT5Xv9NKyIWqLZ0GtQ9w3-JRKIB/view?usp=drive_link"; // cv english
 
   window.open(cvUrl, "_blank");
 };
