7a9afadc43597e03b63a0c3dbdfe8586f7a9f5d5
AyelenIasich <ayeleniasich@gmail.com>
2026-04-26 01:25:19 -0300

    fix: update linkedin URL in socialLinks configuration

--- src/config/socialLinks.js
+++ src/config/socialLinks.js
@@ -1,6 +1,6 @@
 export const socialLinks = {
     github: "https://github.com/AyelenIasich",
-    linkedin: "https://ar.linkedin.com/in/ayeiasich",
+    linkedin: "https://www.linkedin.com/in/ayeiasich/",
     gmail:"iasichayelen@gmail.com"
   };
   
